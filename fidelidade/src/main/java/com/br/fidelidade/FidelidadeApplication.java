16840ac71bfef39c37de4443debd590420a16dd3
Reuel B. de Albuquerque Junior <reuelja@gmail.com>
2022-03-13 22:27:35 -0300

    modulo de fidelidade

---
+++ fidelidade/src/main/java/com/br/fidelidade/FidelidadeApplication.java
@@ -0,0 +1,13 @@
+package com.br.fidelidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidelidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidelidadeApplication.class, args);
+	}
+
+}
