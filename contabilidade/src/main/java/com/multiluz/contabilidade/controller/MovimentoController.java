4ed209575950a331c09dbfda02833391067ce661
Reuel B. de Albuquerque Junior <reuelja@gmail.com>
2022-04-03 18:50:32 -0300

    acerto de codigo

--- contabilidade/src/main/java/com/multiluz/contabilidade/controller/MovimentoController.java
+++ contabilidade/src/main/java/com/multiluz/contabilidade/controller/MovimentoController.java
@@ -9,13 +9,14 @@ import com.multiluz.contabilidade.service.DespesaService;
 import com.multiluz.contabilidade.service.MovimentoService;
 import com.multiluz.contabilidade.service.ReceitaService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
